eb2ec637fb801f880f38add4d18a2d8e53069158
JonasFilho91 <jonas16051991@gmail.com>
2024-09-16 23:29:03 -0300

    logo up

--- src/pages/HomePag.js
+++ src/pages/HomePag.js
@@ -7,10 +7,11 @@ import HeaderOfPage from '../componentes/layouts/HeaderOfPage.jsx';
 //import FooterOfPage from '../componentes/layouts/FooterOfPage.jsx';
 //import ButtonForm from '../componentes/layouts/ButtonForm.jsx';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+//import { useState } from 'react';
 
 function HomePag() {
-    const [index, setIndex] = useState(0);
+   // const [index, setIndex] = useState(0);
+    
 
     return (
         <>
@@ -19,8 +20,8 @@ function HomePag() {
                 <h3>Painel de Controle</h3>
             </div>
             <div className="divNave">
-            <Link className='btn' to="/LoginGerenciaPag"> <button className="btn btn-entrar" onClick={() => setIndex(0)}>Logar</button></Link>
-            <Link className='btn' to="/MonitorAcademiaPag"> <button className="btn btn-entrar" onClick={() => setIndex(1)}>Monitorar Academia</button></Link>
+            <Link className='btn' to="/LoginGerenciaPag"> <button className="btn btn-entrar" >Logar</button></Link>
+            <Link className='btn' to="/MonitorAcademiaPag"> <button className="btn btn-entrar">Monitorar Academia</button></Link>
                 
             </div>
             <div className='divSubCabecalho'>
