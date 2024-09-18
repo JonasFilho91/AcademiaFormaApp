d4b913a12d2d34a624a5ed9d0d3c8aefa53471c8
Lucas Moreira <71292751+lucasmp159@users.noreply.github.com>
2024-09-18 19:38:36 -0300

    Update ClientesLog.js

--- FrontEnd/src/pages/ClientesLog/ClientesLog.js
+++ FrontEnd/src/pages/ClientesLog/ClientesLog.js
@@ -1,11 +1,19 @@
+//import Style
+import HeaderOfPage from '../../componentes/layouts/HeaderOfPage';
+import '../../styles/estilos_JF.css';
+import '../../styles/index.css';
+import ClienteLog from './ClienteLog';
+
 function ClientesLog() {
     return (
         <>
         <h1>ClientesLog</h1>
+        <HeaderOfPage/>
+        <div className="divFlex">
+                <div className="CardCenter">
+                    <ClienteLog/>
+                </div>
+            </div>
        </>
 
     )
-
-}
-
-export default ClientesLog;
