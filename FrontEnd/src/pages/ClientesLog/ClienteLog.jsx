46ef028a38862f99b90c2beed469ae748a7c49ee
Lucas Moreira <71292751+lucasmp159@users.noreply.github.com>
2024-09-18 19:39:57 -0300

    Update ClienteLog.jsx

--- FrontEnd/src/pages/ClientesLog/ClienteLog.jsx
+++ FrontEnd/src/pages/ClientesLog/ClienteLog.jsx
@@ -1,36 +1,37 @@
 //import style
-import '../../../styles/estilos_JF.css';
-import '../../../styles/index.css';
+import '../../styles/estilos_JF.css';
+import '../../styles/index.css';
 
 //import componentes
 //import InputForm from '../../componentes/layouts/InputForm';
 import ButtonForm from '../../componentes/layouts/ButtonForm.jsx';
 import { useForm } from 'react-hook-form';
-import CreateFuncionario from '../../componentes/CRUD/Funcionarios/CreateFuncionario.js';
+import CreateCliente from '../../componentes/CRUD/Cliente/CreateCliente.js';
 
 
-function CadastroFuncionario() {
+function CadastroCliente() {
     const { register, handleSubmit} = useForm();
 
     const onSubmit = (e) => {
-        CreateFuncionario(e);
+        CreateCliente(e);
         
     }
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <div>
-            <h1>Cadastro de Funcionário</h1>
+            <h1>Cadastro de Cliente</h1>
             </div >
             <div>
-            <input class="input" type="text" placeholder="Nome" {...register("Nome")}/>
-            <input class="input" type="number" placeholder="CPF" {...register("CPF")}/>
-            <input class="input" type="text" placeholder="Cargo" {...register("Cargo")}/>
-            <input class="input" type="email" placeholder="Email" {...register("Email")}/>
-            <input class="input" type="text" placeholder="Endereço" {...register("Endereço")}/>
+            <input class="input" type="text" placeholder="Nome_Completo" {...register("Nome_Completo")}/>
+            <input class="input" type="text" placeholder="CPF" {...register("CPF")}/>
+            <input class="input" type="text" placeholder="Idade" {...register("Idade")}/>
+            <input class="input" type="text" placeholder="Peso" {...register("Peso")}/>
+            <input class="input" type="text" placeholder="Plano" {...register("Plano")}/>
+            <input class="input" type="text" placeholder="Endereco" {...register("Endereco")}/>
             <ButtonForm type="submit" Descricao="Cadastrar"/>
             </div>
         </form>
     )
 }
 
-export default CadastroFuncionario;
+export default CadastroCliente;
